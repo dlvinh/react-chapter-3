8f873f7872fbc76bed41c3dc38073e559d8b4191
Duc Vinh Le <vinhxp95@gmail.com>
2022-02-22 16:58:53 +1100

    validate form with new state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,80 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
-
-    state ={
-        firstName: "",
-        lastName: "",
-        userName:"",
-        email:"",
-        password:"",
-        passwordConfirm:"",
+    state = {
+        values: {
+            firstName: "",
+            lastName: "",
+            userName: "",
+            email: "",
+            password: "",
+            passwordConfirm: "",
+        },
+        errors: {
+            firstName: "",
+            lastName: "",
+            userName: "",
+            email: "",
+            password: "",
+            passwordConfirm: "",
+        }
     }
 
     // THis function will take [name and value] of event to set new state for application
-    onInputHandlerS1 =(name, value)=>{
-       // console.log(name,value); console de that name va value la gi
+    onInputHandlerS1 = (name, value) => {
+        // console.log(name,value); console de that name va value la gi
         this.setState(
             {
                 //firstName:value // We can write like this but what if name is not firstName but password => it is inefficent
-                [name]:value // condition of this is name must be correctly same with state properties.
+                [name]: value // condition of this is name must be correctly same with state properties.
             }
-            ,()=>{
+            , () => {
                 console.log(this.state)
-        })
-    } 
+            })
+    }
 
     // WE CAN SHORTEN THIS FUNCTION
-    onInputHandlerS2 = (event)=>{
-        let {name,value} = event.target;
+    onInputHandlerS2 = (event) => {
+        let {name, value} = event.target;
+        let newErrors = {...this.state.errors};
+        let newValues = {...this.state.values};
+
+        // Empty Validation
+        if (value.trim() === ""){
+            newErrors[name] = name + " is required";
+        }else{
+            newErrors[name] = "";
+        }
+
+        // Email Validation
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (name === "email"){
+            if (!re.test(value)){
+                newErrors[name] = name + " is in wrong format";
+            }else{
+                newErrors[name] = "";
+            }
+        }
+
+        //Validate Password
+        const passRe  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        if (name === "password" || name === "passwordConfirm"){
+            if (!passRe.test(value)){
+                newErrors[name] = name + " is in wrong format";
+            }else{
+                newErrors[name] = "";
+            }
+        }
+        
+        
+
+
         this.setState({
-            [name]:value,
-        },()=>{
-            console.log(this.state);
-        }) 
+            values: {...newValues,[name]:value},
+            errors: {...newErrors}
+        }, () => {
+          console.log("state",this.state);
+        })
     }
     render() {
         return (
@@ -41,22 +84,21 @@ export default class Form extends Component {
                     <div className='row'>
                         <div className='col-6'>
                             <div className="group">
-                                <input type="text" required name="firstName" onChange={(event)=>{
-                                   // console.log(event); uncomment to see what event is
-                                    this.onInputHandlerS1(event.target.name,event.target.value);
-                                }}/>
+                                <input type="text" required name="firstName" onChange={this.onInputHandlerS2} />
                                 <span className="highlight" />
                                 <span className="bar" />
                                 <label>First Name</label>
+                                <span className='hightlight text text-danger text-left'>{this.state.errors.firstName}</span>
                             </div>
                         </div>
 
                         <div className='col-6'>
                             <div className="group">
-                                 <input type="text" required name="lastName" onChange={this.onInputHandlerS2}/>  {/*NOTE: we dont need () after function and event param will automatically assigned*/}
+                                <input type="text" required name="lastName" onChange={this.onInputHandlerS2} />  {/*NOTE: we dont need () after function and event param will automatically assigned*/}
                                 <span className="highlight" />
                                 <span className="bar" />
                                 <label>Last Name</label>
+                                <span className='hightlight text text-danger text-left'>{this.state.errors.lastName}</span>
                             </div>
                         </div>
                     </div>
@@ -64,10 +106,11 @@ export default class Form extends Component {
                     <div className='row'>
                         <div className='col'>
                             <div className="group">
-                                <input type="text" required name="userName" onChange={this.onInputHandlerS2}/>
+                                <input type="text" required name="userName" onChange={this.onInputHandlerS2} />
                                 <span className="highlight" />
                                 <span className="bar" />
                                 <label>User Name</label>
+                                <span className='hightlight text text-danger text-left'>{this.state.errors.userName}</span>
                             </div>
                         </div>
 
@@ -76,10 +119,11 @@ export default class Form extends Component {
                     <div className='row'>
                         <div className='col'>
                             <div className="group">
-                                <input type="email" required name="email" onChange={this.onInputHandlerS2}/>
+                                <input type="text" required name="email" onChange={this.onInputHandlerS2} />
                                 <span className="highlight" />
                                 <span className="bar" />
                                 <label>Email</label>
+                                <span className='hightlight text text-danger text-left'>{this.state.errors.email}</span>
                             </div>
                         </div>
                     </div>
@@ -87,23 +131,25 @@ export default class Form extends Component {
                     <div className='row'>
                         <div className='col-6'>
                             <div className="group">
-                                <input type="password" required name="password" onChange={this.onInputHandlerS2}/>
+                                <input type="password" required name="password" onChange={this.onInputHandlerS2} />
                                 <span className="highlight" />
                                 <span className="bar" />
                                 <label>Password</label>
+                                <span className='hightlight text text-danger text-left'>{this.state.errors.password}</span>
                             </div>
                         </div>
 
                         <div className='col-6'>
                             <div className="group">
-                                <input type="password" required name="passwordConfirm" onChange={this.onInputHandlerS2}/>
+                                <input type="password" required name="passwordConfirm" onChange={this.onInputHandlerS2} />
                                 <span className="highlight" />
                                 <span className="bar" />
                                 <label>Password Confirm</label>
+                                <span className='hightlight text text-danger text-left'>{this.state.errors.passwordConfirm}</span>
                             </div>
                         </div>
                     </div>
-                    
+
                     <div>
                         <button type="submit" className="btn text-white bg-dark w-100">Submit</button>
                     </div>
